09f13fda8b60c9dc8e55bb5e5180adbcb20cd22d
Graham Allen <grahamtallen94@gmail.com>
2021-06-10 11:53:09 -0700

    wip

--- init/index.js
+++ init/index.js
@@ -1,1 +1,24 @@
-import json2csv from 'json2csv';
+const csvtojson = require("csvtojson/v2");
+
+const RESTAURANTS_ALL_PATH = "csv/restaurants.csv"
+
+const main = async () => {
+	const restaurantsAll = await csvtojson().fromFile(RESTAURANTS_ALL_PATH);
+
+	// data store for restaraunts by name
+	let byName = {};
+	restaurantsAll.forEach(restaurant => {
+		const { name } = restaurant;
+		const nameLowerCaseNoSpaces = name.toLowerCase().replace(/\s/g, "");
+		restaurant.nameLowerCaseNoSpaces = nameLowerCaseNoSpaces
+		byName[nameLowerCaseNoSpaces] = restaurant;
+	});
+	s
+
+	console.log(byName)
+	
+
+}
+
+main();
+
