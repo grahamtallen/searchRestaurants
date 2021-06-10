7f5ae6b6ad6a7554f932f2c7d144117e7a91c8f1
Graham Allen <grahamtallen94@gmail.com>
2021-06-10 12:02:50 -0700

    Working bucketing for restaraunts

--- init/index.js
+++ init/index.js
@@ -4,7 +4,6 @@ const RESTAURANTS_ALL_PATH = "csv/restaurants.csv"
 
 const main = async () => {
 	const restaurantsAll = await csvtojson().fromFile(RESTAURANTS_ALL_PATH);
-
 	// data store for restaraunts by name
 	let byName = {};
 	restaurantsAll.forEach(restaurant => {
@@ -13,9 +12,40 @@ const main = async () => {
 		restaurant.nameLowerCaseNoSpaces = nameLowerCaseNoSpaces
 		byName[nameLowerCaseNoSpaces] = restaurant;
 	});
-	s
 
-	console.log(byName)
+
+	// Customer rating: bucket every restaruant by customer rating
+	const FiveStarRestaurants = [];
+	const FourStarRestaurants = [];
+	const ThreeStarRestaurants = [];
+	const TwoStarRestaurants = [];
+	const OneStarRestaurants = [];
+	restaurantsAll.forEach(restaurant => {
+		const { customer_rating } = restaurant;
+		switch (customer_rating) {
+			case '5':
+				FiveStarRestaurants.push(restaurant);
+				break;
+			case '4':
+				FourStarRestaurants.push(restaurant);
+				break;
+			case '3':
+				ThreeStarRestaurants.push(restaurant);
+				break;
+			case '2':
+				TwoStarRestaurants.push(restaurant);
+				break;
+			case '1':
+				OneStarRestaurants.push(restaurant);
+				break;
+			default:
+				throw new Error("Unhandled restaurant rating: " + customer_rating)
+		}
+	})
+	console.log({
+		FiveStarRestaurants: FiveStarRestaurants.length,
+		OneStarRestaurants: OneStarRestaurants.length
+	})
 	
 
 }
